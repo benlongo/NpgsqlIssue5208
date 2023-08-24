f615ce50dddddf5f19d7f392e66cb7a71baf3b75
Ben Longo <benlongo9807@gmail.com>
2023-08-24 14:35:21 -0400

    Get rid of ugly delay

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Npgsql.Replication.PgOutput;
 const string connectionString =
     "Server=127.0.0.1;Port=5445;Database=npgsql_test;UserId=npgsql_test;Password=npgsql_test;IncludeErrorDetail=true;";
 
-await using var logicalReplicationConnection = new LogicalReplicationConnection(connectionString);
+var logicalReplicationConnection = new LogicalReplicationConnection(connectionString);
 await logicalReplicationConnection.Open();
 var slot = await logicalReplicationConnection.CreatePgOutputReplicationSlot(
     "test_slot",
@@ -41,15 +41,23 @@ var pgOutputOptions = new PgOutputReplicationOptions(
     twoPhase: false
 );
 
-Task.Delay(500).ContinueWith(async _ => {
+var consumptionTask = ConsumeReplicationStream();
+await EmitMessage(false, "test_prefix", "ping");
+await consumptionTask;
+
+async Task EmitMessage(bool transactional, string prefix, string message) {
     await using var dataSource = dataSourceBuilder.Build();
     await using var conn = await dataSource.OpenConnectionAsync();
     await using var emitCommand =
-        new NpgsqlCommand("SELECT * FROM pg_logical_emit_message(false, 'some_prefix', 'ping')", conn);
+        new NpgsqlCommand(
+            $"SELECT * FROM pg_logical_emit_message({(transactional ? "true" : "false")}, '{prefix}', '{message}')",
+            conn);
     await emitCommand.ExecuteNonQueryAsync();
-});
+}
 
-await foreach (var message in logicalReplicationConnection.StartReplication(slot, pgOutputOptions, default)) {
-    Console.WriteLine($"Received message of type {message.GetType().Name}");
-    logicalReplicationConnection.SetReplicationStatus(message.WalEnd);
+async Task ConsumeReplicationStream() {
+    await foreach (var message in logicalReplicationConnection.StartReplication(slot, pgOutputOptions, default)) {
+        Console.WriteLine($"Received message of type {message.GetType().Name}");
+        logicalReplicationConnection.SetReplicationStatus(message.WalEnd);
+    }
 }
